91f44de50b358ba26596d8641db7f037cd020dae
King Grey <lohitsaidev@gmail.com>
2025-04-18 19:56:08 +0530

    Chore: Add scripts in the package.json

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -1,9 +1,10 @@
-import { arbitrumSepolia, baseSepolia, sepolia } from "viem/chains";
+import { arbitrumSepolia, baseSepolia, monadTestnet, sepolia } from "viem/chains";
 
 export const supportedChains = {
   sepolia: sepolia,
   arbitrum_sepolia: arbitrumSepolia,
   base_sepolia: baseSepolia,
+  monad_testnet: monadTestnet,
 } as const;
 
 export type SupportedChainId = keyof typeof supportedChains;
