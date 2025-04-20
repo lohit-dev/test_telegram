663c06a378bdf9c86b4b96b8467280f13c113813
lohit <lohitsaidev@gmail.com>
2025-04-20 20:07:02 +0530

    Chore: Change viem dependency and add bera and citrea chains

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -1,10 +1,19 @@
-import { arbitrumSepolia, baseSepolia, monadTestnet, sepolia } from "viem/chains";
+import { getBitcoinNetwork } from "@gardenfi/core/dist/src/lib/utils";
+import { Environment } from "@gardenfi/utils";
+import {
+  sepolia,
+  arbitrumSepolia,
+  citreaTestnet,
+  berachainTestnet,
+  baseSepolia,
+} from "viem/chains";
 
 export const supportedChains = {
   sepolia: sepolia,
   arbitrum_sepolia: arbitrumSepolia,
   base_sepolia: baseSepolia,
-  monad_testnet: monadTestnet,
+  citrea_testnet: citreaTestnet,
+  berachain_testnet: berachainTestnet,
 } as const;
 
 export type SupportedChainId = keyof typeof supportedChains;
